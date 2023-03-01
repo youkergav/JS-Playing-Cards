3b48b9f0f5ff5cf148cf09537cb99d1f2f579a3d
Gavin Youker <youkergav@gmail.com>
2023-03-01 02:29:35 -0500

    Cleaned up code Part II

--- javascript/blackjackUtil.js
+++ javascript/blackjackUtil.js
@@ -1,3 +1,4 @@
+// Function to generate universal unique identifiers in UUIDv4 format. WARNING: This is prone to collisions.
 function uuid() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
