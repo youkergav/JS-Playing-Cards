e4cae38af45bf81ddf327c94e84da47b151e7cd3
Gavin Youker <youkergav@gmail.com>
2023-03-01 03:17:05 -0500

    Updated documentation for TODO tree

--- javascript/blackjackElement.js
+++ javascript/blackjackElement.js
@@ -3,7 +3,8 @@ class BlackjackElement extends HTMLElement {
         super();
     }
 
-    // Function to generate universal unique identifiers in UUIDv4 format. WARNING: This is prone to collisions.
+    // Function to generate universal unique identifiers in UUIDv4 format. 
+    // WARNING: This is prone to collisions.
     _uuid() {
         return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
